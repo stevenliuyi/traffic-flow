7d3c5020bef8e60d6186dc8967c58514e25f4309
stevenliuyi <stevenliuyi@gmail.com>
2014-11-27 17:17:17 -0500

    initial version of two-equation PW model

--- main.py
+++ main.py
@@ -11,7 +11,12 @@ def set_mesh():
 # -----------------------------------------------------------------------------
 # define initial condition
 def ic():
-    u = np.ones(nx) * rho0
+    u = np.ones((lmax, nx))
+    if (lmax == 1):
+        u[0,:] *= rho0
+    elif (lmax == 2):
+        u[0,:] *= rho0
+        u[1,:] *= rho0 * (1 - .9*rho0)
     return u
 
 # -----------------------------------------------------------------------------
@@ -44,14 +49,18 @@ def solver():
         e   = flux()
         res = residual(e)
         u  += dt * res
+        #print u[:,1:10]
     return
 
 # -----------------------------------------------------------------------------
 # compute maximum eigenvalue of Jacobi matrix
 def maxlam(u):
     lam = 0.
-    for i in range(0, len(u)):
-        lam = max(lam, abs(vel(u[i])))
+    for i in range(0, nx):
+        if (lmax == 1):
+            lam = max(lam, abs(vel(u[0,i])))
+        elif (lmax == 2):
+            lam = max(lam, abs(u[1,i]/u[0,i])+c0)
     return lam
 
 # -----------------------------------------------------------------------------
@@ -73,8 +82,13 @@ def vel(rho):
 # -----------------------------------------------------------------------------
 # flux vector at a single grid point
 def ee(ui):
-    vi = vel(ui)
-    ei = ui * vi
+    if (lmax == 1):
+        vi = vel(ui)
+        ei = ui * vi
+    elif (lmax == 2):
+        rhoi = ui[0]
+        vi   = ui[1] / ui[0]
+        ei = np.array([rhoi*vi, rhoi*vi**2 + c0**2*rhoi])
     return ei
     
 # -----------------------------------------------------------------------------
@@ -87,43 +101,55 @@ def aa(ui):
 # -----------------------------------------------------------------------------
 # flux vector
 def flux(stage=0):
-    e = np.zeros(nx-1)
+    e = np.zeros((lmax, nx-1))
     for i in range(0,nx-1):
         # Lax method
         if (method == 'lax'):
-            e1 = ee(u[i])
-            e2 = ee(u[i+1])
-            e[i] = .5 * (e1 + e2) - .5 * dx / dt * (u[i+1] - u[i])
+            e1 = ee(u[:,i])
+            e2 = ee(u[:,i+1])
+            e[:,i] = .5 * (e1 + e2) - .5 * dx / dt * (u[:,i+1] - u[:,i])
         # Lax-Wendroff method
         elif (method == 'lax-wendroff'):
-            e1 = ee(u[i])
-            e2 = ee(u[i+1])
-            a1 = aa(u[i])
-            a2 = aa(u[i+1])
+            e1 = ee(u[:,i])
+            e2 = ee(u[:,i+1])
+            a1 = aa(u[:,i])
+            a2 = aa(u[:,i+1])
             a  = .5 * (a1 + a2)
-            e[i] = .5 * (e1 + e2) - .5 * dt / dx * a * (e2 - e1)
+            e[:,i] = .5 * (e1 + e2) - .5 * dt / dx * a * (e2 - e1)
         # MacCormack method
         elif (method == 'maccormack'):
             if (stage == 0):
-                e[i] = ee(u[i+1])
+                e[:,i] = ee(u[:,i+1])
             elif (stage == 1):
-                e[i] = ee(u[i])
+                e[:,i] = ee(u[:,i])
         # Jameson 4-stage Runga-Kutta
         elif (method == 'rk4'):
-            e1 = ee(u[i])
-            e2 = ee(u[i+1])
-            e[i] = .5 * (e1 + e2)
+            e1 = ee(u[:,i])
+            e2 = ee(u[:,i+1])
+            e[:,i] = .5 * (e1 + e2)
 
     # artificial viscosity
     if (avmodel): e = av(e)
     return e
 
 # -----------------------------------------------------------------------------
+# source vector
+def source(u):
+    s = np.zeros((lmax,nx))
+    for i in range(0, nx):
+        rhoi   = u[0,i]
+        vi     = u[1,i] / u[0,i]
+        s[0,i] = 0.
+        s[1,i] = rhoi * ((1 - .9 * rhoi) - vi) / 2.
+    return s
+
+# -----------------------------------------------------------------------------
 # residual
 def residual(e):
-    res = np.zeros(nx)
+    res = np.zeros((lmax,nx))
     for i in range(1, nx-1):
-        res[i] = -(e[i] - e[i-1]) / dx
+        res[:,i] = -(e[:,i] - e[:,i-1]) / dx
+    #if (lmax == 2): res += source(u)
     return res
 
 # -----------------------------------------------------------------------------
@@ -133,33 +159,40 @@ def av(e):
     lam0 = maxlam(u)
     u0   = .5
     for i in range(1,nx-2):
-        du   = u[i+1] - u[i]
-        d3u  = u[i+2] - 3*u[i+1] + 3*u[i] - u[i-1]
+        du   = u[:,i+1] - u[:,i]
+        d3u  = u[:,i+2] - 3*u[:,i+1] + 3*u[:,i] - u[:,i-1]
         eps2 = kappa2 * abs(du) / u0
         eps4 = kappa4
-        e[i] -= (eps2 * du - eps4 * d3u) * lam0
+        e[:,i] -= (eps2 * du - eps4 * d3u) * lam0
     return e
 
+# -----------------------------------------------------------------------------
+# parameters
 xmin = 0
 xmax = 50
-nx   = 201     # number of grid points
+nx   = 101     # number of grid points
 
-rho0 = 0.2
+rho0 = 0.8
 fr   = 0.2
 cfl  = 0.5
 imax = 1000
 eps  = 1e-5
 tmax = 20
 
+# -----------------------------------------------------------------------------
 # traffic flow model
-# acceptable values: lwr-greenshield, lwr-greenberg, lwr-underwood
-model = 'lwr-underwood'
+# acceptable values: lwr-greenshield, lwr-greenberg, lwr-underwood, pw
+model = 'lwr-greenshield'
+model = 'pw'
+c0 = .5
+lmax  = 2 if (model == 'pw') else 1
 
+# -----------------------------------------------------------------------------
 # numerical methods
 # acceptable values: lax, lax-wendroff, maccormack, rk4
-method  = 'maccormack'
+method  = 'lax'
 
-avmodel = True
+avmodel = False
 kappa2  = 2.
 kappa4  = 0.2
 
@@ -177,25 +210,37 @@ for i in range(0, imax):
     dt = step()
 
     solver()
-    maxres = max(abs(res))
+
+    maxres = max(abs(res[0,]))
     # if (maxres < 1e-5): break
     time  += dt
     if (time < tmax * fr):
         color = 'r'
-        u[0] = 0.
+        if (lmax == 1):
+            u[0,0] = 0.
+        elif (lmax == 2):
+            u[0,0] = 0.01
+            if (u[1,1]/u[0,1]<c0):
+                u[1,0] = u[1,1]
+            else:
+                u[1,0] = u[0,0]*(1-.9*u[0,0])
     else:
         color = 'g'
-        u[0] = rho0
-    u[-1] = u[-2]
+        if (lmax == 1):
+            u[0,0] = rho0
+        elif (lmax == 2):
+            u[0,0] = rho0
+            u[1,0] = u[1,1]
+    u[:,-1] = u[:,-2]
 
     if (time > tmax): time = 0
     
     if (i == 0):
-        line1, = ax.plot(x, u,'-o')
+        line1, = ax.plot(x, u[0,],'-o')
         line1.set_color(color)
         ax.set_ylim(0,1)
         fig.show()
     else:
-        line1.set_ydata(u)
+        line1.set_ydata(u[0,])
         line1.set_color(color)
         fig.canvas.draw()
